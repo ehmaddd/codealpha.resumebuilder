6264eac7de598de6bdfdbbd5ec2c637f2a93a97c
ehmaddd <ehmaddd@gmail.com>
2023-10-14 22:51:23 +0500

    Add style to personal details

--- src/App.js
+++ src/App.js
@@ -46,32 +46,37 @@ function App() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Personal Information</h2>
-      <label>Name: <input type="text" name="name" value={value.name} onChange={(e) => setValue({ ...value, name: e.target.value })} /></label>
-      <label>Title: <input type="text" name="title" value={value.title} onChange={(e) => setValue({ ...value, title: e.target.value })} /></label>
-      <label>Summary: <textarea name="summary" value={value.summary} onChange={(e) => setValue({ ...value, summary: e.target.value })} /></label>
-      <label>GitHub: <input type="text" name="github" value={value.github} onChange={(e) => setValue({ ...value, github: e.target.value })} /></label>
-      <label>Twitter: <input type="text" name="twitter" value={value.twitter} onChange={(e) => setValue({ ...value, twitter: e.target.value })} /></label>
-      <label>LinkedIn: <input type="text" name="linkedin" value={value.linkedin} onChange={(e) => setValue({ ...value, linkedin: e.target.value })} /></label>
+      <div class="personal-div">
+        <h2>Personal Information</h2>
+        <label class="personal-label">Name: </label><input type="text" name="name" value={value.name} onChange={(e) => setValue({ ...value, name: e.target.value })} />
+        <label class="personal-label">Title: </label><input type="text" name="title" value={value.title} onChange={(e) => setValue({ ...value, title: e.target.value })} />
+        <label class="personal-label">Summary: </label><textarea name="summary" value={value.summary} onChange={(e) => setValue({ ...value, summary: e.target.value })} />
+        <label class="personal-label">GitHub: </label><input type="text" name="github" value={value.github} onChange={(e) => setValue({ ...value, github: e.target.value })} />
+        <label class="personal-label">Twitter: </label><input type="text" name="twitter" value={value.twitter} onChange={(e) => setValue({ ...value, twitter: e.target.value })} />
+        <label class="personal-label">LinkedIn: </label><input type="text" name="linkedin" value={value.linkedin} onChange={(e) => setValue({ ...value, linkedin: e.target.value })} />
+      </div>
 
-      <h2>Skills</h2>
-      <ul>
-        {value.skills.map((skill, index) => (
-          <li key={index}>
-            <input
-              type="text"
-              value={skill}
-              readOnly
-            />
-            <button>Remove</button>
-          </li>
-        ))}
-      </ul>
-      <label>
+      <div class="skill-experience">
+        <div class="skills-div">
+          <h2>Skills</h2>
+            <ul>
+              {value.skills.map((skill, index) => (
+                <li key={index}>
+                <input
+                  type="text"
+                  value={skill}
+                  readOnly
+                />
+                 <button>Remove</button>
+                </li>
+              ))}
+            </ul>
+        <label>
         New Skill:
         <input type="text" name="skill" value={skill} onChange={(e) => setSkill(e.target.value)} />
       </label>
       <button onClick={addSkill}>Add Skill</button>
+        </div>
 
       <h2>Experience</h2>
       <ul>
@@ -91,6 +96,7 @@ function App() {
         <input type="text" name="experience" value={exper} onChange={(e) => setExper(e.target.value)} />
       </label>
       <button onClick={addExper}>Add Experience</button>
+      </div>
 
       <h2>Education</h2>
       {/* Similar structure for education section */}
