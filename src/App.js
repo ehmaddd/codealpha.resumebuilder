bee43be44fefac3add1de794769a77453b253ccc
ehmaddd <ehmaddd@gmail.com>
2023-10-15 22:36:45 +0500

    Add social links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [skill, setSkill] = useState('');
   const [exper, setExper] = useState('');
+  const [edu, setEdu] = useState({degree: '', subject: ''});
   const [value, setValue] = useState({
     name: '',
     title: '',
@@ -40,6 +41,23 @@ function App() {
     setExper('');
   };
 
+  const addEdu = (event) => {
+    const degre = event.target.value;
+    setEdu((prevEdu) => ({
+      ...prevEdu,
+      degree: degre,
+    }));
+  }
+
+  const addSub = (event) => {
+    const sub = event.target.value;
+    setEdu((prevEdu) => ({
+      ...prevEdu,
+      subject: sub,
+    }));
+  };
+
+
   const delSkill = (index) => {
     setValue((prevState) => ({
       ...prevState,
@@ -66,9 +84,6 @@ function App() {
           <input type="text" name="title" value={value.title} placeholder="Title" onChange={(e) => setValue({ ...value, title: e.target.value })} />
           <input type="text" name="name" value={value.name} placeholder="Name" onChange={(e) => setValue({ ...value, name: e.target.value })} />
           <textarea name="summary" value={value.summary} placeholder="Summary" onChange={(e) => setValue({ ...value, summary: e.target.value })} />
-          <input type="text" name="github" value={value.github} placeholder="Github" onChange={(e) => setValue({ ...value, github: e.target.value })} />
-          <input type="text" name="twitter" value={value.twitter} placeholder="Twitter" onChange={(e) => setValue({ ...value, twitter: e.target.value })} />
-          <input type="text" name="linkedin" value={value.linkedin} placeholder="LinkedIn" onChange={(e) => setValue({ ...value, linkedin: e.target.value })} />
         </div>
 
         <div class="skills-div">
@@ -109,9 +124,39 @@ function App() {
           <button class="btn" onClick={addExper}>Add Experience</button>
         </div>
       </div>
+      <div class="top-div">
+        <div class="education-div">
+          <h2>Education</h2>
+          <select class="degree" onChange={addEdu}>
+            <option value="PhD">PhD</option>
+            <option value="MS">MS</option>
+            <option value="MSc">M.Sc</option>
+            <option value="MA">M.A</option>
+            <option value="BSc">B.Sc</option>
+            <option value="BA">B.A</option>
+            <option value="FSc">F.Sc</option>
+            <option value="FA">F.A</option>
+          </select>
+          <select class="subject" onChange={addSub}>
+            <option value="Arabic">Arabic</option>
+            <option value="Biology">Biology</option>
+            <option value="Computer">Computer</option>
+            <option value="Chemistry">Chemistry</option>
+            <option value="Geography">Geography</option>
+            <option value="Math">Math</option>
+            <option value="Physics">Physics</option>
+            <option value="Urdu">Urdu</option>
+          </select>
+        </div>
 
-      <h2>Education</h2>
-      {/* Similar structure for education section */}
+        <div class="social-div">
+          <h2>Social Links</h2>
+          <input type="text" name="github" value={value.github} placeholder="Github" onChange={(e) => setValue({ ...value, github: e.target.value })} />
+          <input type="text" name="twitter" value={value.twitter} placeholder="Twitter" onChange={(e) => setValue({ ...value, twitter: e.target.value })} />
+          <input type="text" name="linkedin" value={value.linkedin} placeholder="LinkedIn" onChange={(e) => setValue({ ...value, linkedin: e.target.value })} />
+        </div>
+      </div>
+
       <button type="submit">Save</button>
     </form>
   );
