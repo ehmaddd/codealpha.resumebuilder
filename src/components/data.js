517c2cccce684fdce19a7b445f75aaacaa65f4cb
ehmaddd <ehmaddd@gmail.com>
2023-11-10 15:31:54 +0500

    Update data array structure

--- src/components/data.js
+++ src/components/data.js
@@ -1,30 +1,42 @@
 const data = [
-    {
-      personalinfo: {
-        fullName: "",
-        contactNumber: "",
-        emailAddress: "",
-        physicalAddress: "",
-        linkedIn: "",
-        websiteAddress: "",
+  {
+    personalinfo: {
+      fullName: "",
+      contactNumber: "",
+      emailAddress: "",
+      physicalAddress: "",
+      linkedIn: "",
+      websiteAddress: "",
+    },
+  },
+  {
+    summary: '',
+  },
+  {
+    skills: {
+      yearsOfExperience: "null",
+      expertiseDomains: {
+        frontEnd: false,
+        backEnd: false,
+        databases: false,
+        algorithms: false,
+        architectures: false,
       },
+      otherSkills: "",
     },
-    {
-      summary: ''
-    },
-    {
-      experience: {
-        yearsOfExperience: "null",
-        expertiseDomains: {
-          frontEnd: false,
-          backEnd: false,
-          databases: false,
-          algorithms: false,
-          architectures: false,
-        },
-        otherSkills: "",
+  },
+  {
+    workExperiences: [
+      {
+        company: "",
+        jobTitle: "",
+        location: "",
+        startDate: "",
+        endDate: "",
+        responsibilities: [],
       },
-    },
-  ];
+    ],
+  },
+];
 
 export default data;
