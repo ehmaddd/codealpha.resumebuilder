5807447c4e118627b01425486ba8a16588df19cb
ehmaddd <ehmaddd@gmail.com>
2023-11-12 19:33:53 +0500

    Created UI

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,61 @@ import './Skills.css';
 const Skills = () => {
   const [technicalSkills, setTechnicalSkills] = useState(data[4]?.skills?.technicalSkills || []);
   const [softSkills, setSoftSkills] = useState(data[4]?.skills?.softSkills || []);
+
+  return (
+    <div className="main-div">
+      <h1>Skills</h1>
+      <div className="container-div">
+        <div>
+          <h2>Technical Skills</h2>
+          {technicalSkills.map((skill, index) => (
+            <div key={index} className="skill">
+              <input
+                type="text"
+                value={skill}
+                onChange={(e) => handleInputChange('technical', index, e.target.value)}
+              />
+              <button type="button" onClick={() => handleRemoveSkill('technical', index)}>
+                Remove
+              </button>
+            </div>
+          ))}
+          <button type="button" onClick={() => handleAddSkill('technical')}>
+            Add Technical Skill
+          </button>
+        </div>
+        <div>
+          <h2>Soft Skills</h2>
+          {softSkills.map((skill, index) => (
+            <div key={index} className="skill">
+              <input
+                type="text"
+                value={skill}
+                onChange={(e) => handleInputChange('soft', index, e.target.value)}
+              />
+              <button type="button" onClick={() => handleRemoveSkill('soft', index)}>
+                Remove
+              </button>
+            </div>
+          ))}
+          <button type="button" onClick={() => handleAddSkill('soft')}>
+            Add Soft Skill
+          </button>
+        </div>
+      </div>
+      <div className="buttons">
+        <Link to="/summary" className="backButton" onClick={handleSaveData}>
+          BACK
+        </Link>
+        <button className="saveButton" onClick={handleSaveData}>
+          SAVE
+        </button>
+        <Link to="/nextPage" className="nextButton" onClick={handleSaveData}>
+          NEXT
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
