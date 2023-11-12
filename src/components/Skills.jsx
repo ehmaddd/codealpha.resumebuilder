f0a6fe9ef5c0e54f96227a36df55b4ba955e4e32
ehmaddd <ehmaddd@gmail.com>
2023-11-12 19:33:14 +0500

    Create state variables

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,6 +4,8 @@ import data from './data';
 import './Skills.css';
 
 const Skills = () => {
+  const [technicalSkills, setTechnicalSkills] = useState(data[4]?.skills?.technicalSkills || []);
+  const [softSkills, setSoftSkills] = useState(data[4]?.skills?.softSkills || []);
 }
 
 export default Skills;
