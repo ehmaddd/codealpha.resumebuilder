b54bd4b8b96133b3f7768aaaba820a35e7452b23
ehmaddd <ehmaddd@gmail.com>
2023-11-19 23:51:36 +0500

    Update GeneratePDF File

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ import Experience from './Experience';
 import Education from './Education';
 import Skills from './Skills';
 import Certifications from './Certifications';
+import GeneratePDF from './GeneratePDF';
 import './navbar.css';
 
 const Navbar = () => {
@@ -29,6 +30,7 @@ const Navbar = () => {
         <Route path="/edu" element={<Education />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/cert" element={<Certifications />} />
+        <Route path="/pdf" element={<GeneratePDF />} />
       </Routes>
     </>
   );
