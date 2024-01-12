5efcfe8c67f4600aeaee1cbc28464601a8fc3661
ehmaddd <ehmaddd@gmail.com>
2024-01-12 17:54:30 +0500

    Adjust personal info

--- src/components/GeneratePDF.js
+++ src/components/GeneratePDF.js
@@ -12,36 +12,44 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   heading: {
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: 'bold',
     marginBottom: 8,
     textDecoration: 'underline',
   },
   content: {
-    fontSize: 14,
+    fontSize: 12,
     marginBottom: 4,
   },
 });
 
 const MyDocument = () => {
   const { personalinfo } = data[0];
-  const { summary } = data[1];
-  const { workExperiences } = data[2];
-  const { education } = data[3];
-  const { skills } = data[4];
-  const { certifications } = data[5];
+  // const { summary } = data[1];
+  // const { workExperiences } = data[2];
+  // const { education } = data[3];
+  // const { skills } = data[4];
+  // const { certifications } = data[5];
   console.log(personalinfo);
-  console.log(summary);
-  console.log(workExperiences);
-  console.log(education);
-  console.log(skills);
-  console.log(certifications);
+  // console.log(summary);
+  // console.log(workExperiences);
+  // console.log(education);
+  // console.log(skills);
+  // console.log(certifications);
   return (
   <Document>
     <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
+    <View style={styles.section}>
+          <Text style={styles.heading}>Contact Information</Text>
+          <View>
+            <Text style={styles.content}>Full Name: {personalinfo.fullName}</Text>
+            <Text style={styles.content}>Contact Number: {personalinfo.contactNumber}</Text>
+            <Text style={styles.content}>Email Address: {personalinfo.emailAddress}</Text>
+            <Text style={styles.content}>Physical Address: {personalinfo.physicalAddress}</Text>
+            <Text style={styles.content}>LinkedIn: {personalinfo.linkedIn}</Text>
+            <Text style={styles.content}>Website: {personalinfo.websiteAddress}</Text>
+          </View>
+        </View>
       <View style={styles.section}>
         <Text>Section #2</Text>
       </View>
