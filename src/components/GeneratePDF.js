fbd052dbb9fb3f149f1c98ef13c1bcbe3aceb0b5
ehmaddd <ehmaddd@gmail.com>
2024-01-22 23:50:47 +0500

    Display education

--- src/components/GeneratePDF.js
+++ src/components/GeneratePDF.js
@@ -67,7 +67,7 @@ const MyDocument = () => {
   const { personalinfo } = data[0];
   const { summary } = data[1];
   const { workExperiences } = data[2];
-  // const { education } = data[3];
+  const { education } = data[3];
   // const { skills } = data[4];
   // const { certifications } = data[5];
   // console.log(personalinfo);
