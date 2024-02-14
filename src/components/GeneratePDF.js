8d356da49e692e1a9d95c518caafac74f16da1b4
ehmaddd <ehmaddd@gmail.com>
2024-02-14 21:28:24 +0500

    Update degree major location

--- src/components/GeneratePDF.js
+++ src/components/GeneratePDF.js
@@ -197,7 +197,8 @@ const MyDocument = () => {
                 <Text style={styles.header2}>• {education.institution}</Text>
                 <Text style={styles.content}>{new Date(education.graduationDate).toLocaleDateString('en-GB')}</Text>
               </View>
-              <Text style={styles.designation}>{education.degree} ({education.major})</Text>
+              <Text style={styles.designation}>{education.degree}</Text>
+              <Text style={styles.designation}>{education.major}</Text>
             </View>
           ))}
         </View>
